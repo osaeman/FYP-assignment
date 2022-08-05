03c4081f9c760ec48f9cad2ca15a8e0339627c50
osaeman <2015n2424@gmail.com>
2022-08-04 23:27:52 -0700

    first commit

---
+++ javascript/functionality.js
@@ -0,0 +1,6 @@
+// Mobile version navbar
+const hamburger_icon = document.querySelector(".hamburger_icon");
+const hamburger_menu = document.querySelector(".hamburger_menu");
+hamburger_icon.addEventListener("click", () => {
+  hamburger_menu.classList.toggle("hamburger_menu2");
+});
